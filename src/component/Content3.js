ef2d9813777beeef4233d70892caf3130aae8d35
datfc97pro <datfc97pro@gmail.com>
2019-01-04 22:37:17 +0700

    update

--- src/component/Content3.js
+++ src/component/Content3.js
@@ -16,6 +16,66 @@ class Content3 extends Component {
             <div className="col-md-3 mb-5">
               <div className="card" style={{ width: "100%" }}>
                 <a
+                  href="https://bwd-react.herokuapp.com/"
+                  style={{ background: "#282c34" }}
+                  target="_b_blank"
+                >
+                  <img
+                    className="card-img-top img-fluid"
+                    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+                    alt=""
+                    style={{
+                      height: "250px"
+                    }}
+                  />
+                </a>
+                <div className="card-body">
+                  <p
+                    className="card-text"
+                    style={{
+                      fontSize: "25px",
+                      fontWeight: "400"
+                    }}
+                  >
+                    BookWithĐạt - React - Nodejs
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3 mb-5">
+              <div className="card" style={{ width: "100%" }}>
+                <a
+                  href="https://bwd-angular.herokuapp.com/"
+                  style={{ background: "#282c34" }}
+                  target="_b_blank"
+                >
+                  <img
+                    className="card-img-top img-fluid"
+                    style={{
+                      height: "250px"
+                    }}
+                    src="https://cdn.worldvectorlogo.com/logos/angular-icon-1.svg"
+                    alt=""
+                  />
+                </a>
+                <div className="card-body">
+                  <p
+                    className="card-text"
+                    style={{
+                      fontSize: "25px",
+                      fontWeight: "400"
+                    }}
+                  >
+                    BookWithĐạt - Angular - Nodejs
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3 mb-5">
+              <div className="card" style={{ width: "100%" }}>
+                <a
                   href="https://react-ngodat97.herokuapp.com/"
                   style={{ background: "#282c34" }}
                   target="_b_blank"
@@ -42,6 +102,7 @@ class Content3 extends Component {
                 </div>
               </div>
             </div>
+
             <div className="col-md-3 mb-5">
               <div className="card" style={{ width: "100%" }}>
                 <a
@@ -71,6 +132,7 @@ class Content3 extends Component {
                 </div>
               </div>
             </div>
+
             <div className="col-md-3 mb-5">
               <div className="card" style={{ width: "100%" }}>
                 <a
@@ -100,15 +162,17 @@ class Content3 extends Component {
                 </div>
               </div>
             </div>
+
             <div className="col-md-3 mb-5">
               <div className="card" style={{ width: "100%" }}>
                 <a
                   href="https://checklogin-react-router-firebase.netlify.com/"
+                  style={{ background: "#282c34" }}
                   target="_b_blank"
                 >
                   <img
                     className="card-img-top img-fluid"
-                    src="https://i.imgur.com/pD4k6ox.jpg?1"
+                    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
                     alt=""
                     style={{
                       height: "250px"
@@ -123,24 +187,22 @@ class Content3 extends Component {
                       fontWeight: "400"
                     }}
                   >
-                    React - FireBase - Test check login
+                    BookWithĐạt - React - Nodejs
                   </p>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="row">
+
             <div className="col-md-3 mb-5">
               <div className="card" style={{ width: "100%" }}>
                 <a
                   href="https://reactjs-redux-firebase.netlify.com/"
+                  style={{ background: "#282c34" }}
                   target="_b_blank"
                 >
                   <img
                     className="card-img-top img-fluid"
-                    src="https://i.imgur.com/pD4k6ox.jpg?1"
+                    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
                     alt=""
                     style={{
                       height: "250px"
@@ -155,7 +217,7 @@ class Content3 extends Component {
                       fontWeight: "400"
                     }}
                   >
-                    React - FireBase - AHIHIHIHI
+                    BookWithĐạt - React - Nodejs
                   </p>
                 </div>
               </div>
