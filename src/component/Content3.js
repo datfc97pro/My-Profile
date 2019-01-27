2a8aaad58b51ebdf379a9f294e9d940ef5754787
datfc97pro <datfc97pro@gmail.com>
2019-01-27 10:12:32 +0700

    update

--- src/component/Content3.js
+++ src/component/Content3.js
@@ -16,6 +16,36 @@ class Content3 extends Component {
             <div className="col-md-3 mb-5">
               <div className="card" style={{ width: "100%" }}>
                 <a
+                  href="https://vuejs-share.herokuapp.com/"
+                  style={{ background: "#282c34" }}
+                  target="_b_blank"
+                >
+                  <img
+                    className="card-img-top img-fluid"
+                    src="https://cdn.worldvectorlogo.com/logos/vue-js-1.svg"
+                    alt=""
+                    style={{
+                      height: "250px"
+                    }}
+                  />
+                </a>
+                <div className="card-body">
+                  <p
+                    className="card-text"
+                    style={{
+                      fontSize: "25px",
+                      fontWeight: "400"
+                    }}
+                  >
+                    VueShare - Nodejs - Graphql
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3 mb-5">
+              <div className="card" style={{ width: "100%" }}>
+                <a
                   href="https://bwd-react.herokuapp.com/"
                   style={{ background: "#282c34" }}
                   target="_b_blank"
@@ -188,66 +218,6 @@ class Content3 extends Component {
                     }}
                   >
                     Invoices - Angular - Nodejs
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-3 mb-5">
-              <div className="card" style={{ width: "100%" }}>
-                <a
-                  href="https://checklogin-react-router-firebase.netlify.com/"
-                  style={{ background: "#282c34" }}
-                  target="_b_blank"
-                >
-                  <img
-                    className="card-img-top img-fluid"
-                    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
-                    alt=""
-                    style={{
-                      height: "250px"
-                    }}
-                  />
-                </a>
-                <div className="card-body">
-                  <p
-                    className="card-text"
-                    style={{
-                      fontSize: "25px",
-                      fontWeight: "400"
-                    }}
-                  >
-                    BookWithĐạt - React - Nodejs
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-3 mb-5">
-              <div className="card" style={{ width: "100%" }}>
-                <a
-                  href="https://reactjs-redux-firebase.netlify.com/"
-                  style={{ background: "#282c34" }}
-                  target="_b_blank"
-                >
-                  <img
-                    className="card-img-top img-fluid"
-                    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
-                    alt=""
-                    style={{
-                      height: "250px"
-                    }}
-                  />
-                </a>
-                <div className="card-body">
-                  <p
-                    className="card-text"
-                    style={{
-                      fontSize: "25px",
-                      fontWeight: "400"
-                    }}
-                  >
-                    BookWithĐạt - React - Nodejs
                   </p>
                 </div>
               </div>
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer className="container-fluid">
-                <div className="row">
-                    <p style={{ margin: 'auto' }}>Copy right © 2018 by NgoDat. All rights reserved</p>
-                </div>
-            </footer>
+  render() {
+    const date = new Date();
 
-
-        );
-    }
+    return (
+      <footer className="container-fluid">
+        <div className="row">
+          <p style={{ margin: "auto" }}>
+            Copy right © {date.getFullYear()} by NgoDat. All rights reserved
+          </p>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
