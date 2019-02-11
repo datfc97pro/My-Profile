5dbc474057b19c984cdfc0eb24dc362bc78f55b9
datfc97pro <datfc97pro@gmail.com>
2019-02-11 22:30:18 +0700

    update

--- src/component/Content3.js
+++ src/component/Content3.js
@@ -222,6 +222,39 @@ class Content3 extends Component {
                 </div>
               </div>
             </div>
+
+
+            <div className="col-md-3 mb-5">
+              <div className="card" style={{ width: "100%" }}>
+                <a
+                  href="https://graphql-react-ngodat97.herokuapp.com/"
+                  style={{ background: "#282c34" }}
+                  target="_b_blank"
+                >
+                  <img
+                    className="card-img-top img-fluid"
+                    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+                    alt=""
+                    style={{
+                      height: "250px"
+                    }}
+                  />
+                </a>
+                <div className="card-body">
+                  <p
+                    className="card-text"
+                    style={{
+                      fontSize: "25px",
+                      fontWeight: "400"
+                    }}
+                  >
+                    React - Nodejs - Graphql - Mongodb
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
           </div>
         </div>
       </section>
